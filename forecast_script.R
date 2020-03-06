9b9e70a7f6d621e8b21bf331b2cb91b2a89b6008
Peer Christensen <pech@saxo.com>
2020-03-06 16:35:43 +0100

    add quote

--- forecast_script.R
+++ forecast_script.R
@@ -12,7 +12,7 @@
 # 5. tilgang
 # 6. afgang
 
-# månedlige beregninger
+# mdlige beregninger
 
 # 7. Omsætning (2.333 * antal betalende)
 # 8. Gennemsnitlig streamingcost pr. bog ( (streaming buckets * 29) /100 )
@@ -24,16 +24,19 @@
 
 library(RODBC)
 library(tidyverse)
+library(lubridate)
 library(forecast)
 library(tsibble)
 library(openxlsx)
 library(emayili)
+library(fable)
+library(feasts)
 
 # -----------------------------------------------------------------------------------------
 # SQL login
 # -----------------------------------------------------------------------------------------
 
-credentials <- read_rds("credentials.rds")
+credentials <- read_rds("C:/Users/pech/Desktop/RProjects/ForecastAbo/credentials.rds")
 
 channel <- odbcConnect(credentials[1],credentials[2],credentials[3])
 
@@ -120,7 +123,7 @@ query4 <- "SELECT TOP 1000
 df1 <- sqlQuery(channel,query1)
 
 df2 <- sqlQuery(channel,query2) %>%
-  mutate(Period  = fct_recode(as.factor(Period), Paid = "1", Free = "2")) %>%
+  mutate(Period  = fct_recode(as.factor(Period), Betalende = "1", Gratis = "2")) %>%
   pivot_wider(names_from = Period, values_from = nSubscribers)
 
 df3 <- sqlQuery(channel,query3) %>%
@@ -137,7 +140,7 @@ df <- df2 %>%
   as_tibble() %>%
   mutate(Date = as.Date(as.character(Date), format = "%Y%m%d")) %>%
   filter(Date < today()) %>%
-  mutate(TotalMembers = Paid + Free)
+  mutate(SamletBestand = Betalende + Gratis)
 
 # # check Date gaps
 # df %>%
@@ -179,23 +182,23 @@ fc_streaming <- fit_streaming %>%
 
 # fit ARIMA model
 fit_betalende <- df_ts %>%
-  model(arima = ARIMA(Paid))
+  model(arima = ARIMA(Betalende))
 
 # calculate forecasts
 fc_betalende <- fit_betalende %>% 
   forecast(h=as.numeric(as.Date("2020-12-31") - today()+1)) %>%
   hilo() %>% 
   as_tibble() %>%
-  rename(Paid_ci80 = `80%`, Paid_ci95 = `95%`) %>%
+  rename(Betalende_ci80 = `80%`, Betalende_ci95 = `95%`) %>%
   select(-.model)
 
-# slut antal betalende per måned
-betalende_måned <- fc_betalende %>%
+# slut antal betalende per md
+betalende_md <- fc_betalende %>%
   group_by(month = month(Date)) %>%
   filter(Date == max(Date)) %>%
   ungroup() %>%
   mutate(month = month.name[month]) %>%
-  select(month, Betalende = Paid)
+  select(month, Betalende)
 
 # -----------------------------------------------------------------------------------------
 # Forecast Antal medlemmer (samlet)
@@ -203,23 +206,23 @@ betalende_måned <- fc_betalende %>%
 
 # fit ARIMA model
 fit_samlet <- df_ts %>%
-  model(arima = ARIMA(TotalMembers))
+  model(arima = ARIMA(SamletBestand))
 
 # calculate forecasts
 fc_samlet <- fit_samlet %>% 
   forecast(h=as.numeric(as.Date("2020-12-31") - today()+1)) %>%
   hilo() %>% 
   as_tibble() %>%
-  rename(TotalMembers_ci80 = `80%`, TotalMembers_ci95 = `95%`) %>%
+  rename(SamletBestand_ci80 = `80%`, SamletBestand_ci95 = `95%`) %>%
   select(-.model)
 
-# slut antal medlemmer per måned
-bestand_måned <- fc_samlet %>%
+# slut antal medlemmer per md
+bestand_md <- fc_samlet %>%
   group_by(month = month(Date)) %>%
   filter(Date == max(Date)) %>%
   ungroup() %>%
   mutate(month = month.name[month]) %>%
-  select(month, Bestand = TotalMembers)
+  select(month, SamletBestand)
 
 # -----------------------------------------------------------------------------------------
 # Forecast Tilgang
@@ -266,8 +269,8 @@ fc_start <- fit_start %>%
   select(-.model,-.distribution) %>%
   rename(tilgang = Start)
 
-# tilgang (sum) per måned
-tilgang_måned <- fc_start  %>%
+# tilgang (sum) per md
+tilgang_md <- fc_start  %>%
   group_by(month = month(Date)) %>%
   summarise(tilgang = sum(tilgang)) %>%
   mutate(month = month.name[month]) 
@@ -323,8 +326,8 @@ fc_end <- fit_end %>%
   select(-.model, -.distribution) %>%
   rename(afgang = End)
 
-# afgang (sum) per måned
-afgang_måned <- fc_end  %>%
+# afgang (sum) per md
+afgang_md <- fc_end  %>%
   group_by(month = month(Date)) %>%
   summarise(afgang = sum(afgang)) %>%
   mutate(month = month.name[month]) 
@@ -333,17 +336,17 @@ afgang_måned <- fc_end  %>%
 # Beregning af Samlet antal antal gratis medlemmer
 # -----------------------------------------------------------------------------------------
 
-gratis <- fc_samlet$TotalMembers - fc_betalende$Paid
+Gratis <- fc_samlet$SamletBestand - fc_betalende$Betalende
 
 # -----------------------------------------------------------------------------------------
 # Beregning af omsætning
 # -----------------------------------------------------------------------------------------
 
 revenue <- fc_betalende %>%
-  mutate(revenue = Paid * 2.33) %>%
-  select(Date,revenue) %>%
+  mutate(Revenue = Betalende * 2.33) %>%
+  select(Date,Revenue) %>%
   group_by(month =month(Date)) %>%
-  summarise(revenue = sum(revenue)) %>%
+  summarise(Revenue = sum(Revenue)) %>%
   mutate(month = month.name[month])
   
 # -----------------------------------------------------------------------------------------
@@ -351,10 +354,10 @@ revenue <- fc_betalende %>%
 # -----------------------------------------------------------------------------------------
 
 streaming_cost <- fc_streaming %>%
-  mutate(cost = (nBuckets * 29) / 100) %>%
-  select(Date, cost) %>%
+  mutate(Cost = (nBuckets * 29) / 100) %>%
+  select(Date, Cost) %>%
   group_by(month =month(Date)) %>%
-  summarise(cost = sum(cost)) %>%
+  summarise(Cost = sum(Cost)) %>%
   mutate(month = month.name[month])
 
 # -----------------------------------------------------------------------------------------
@@ -372,16 +375,16 @@ start_medlemmer <- fc_samlet %>%
   ungroup() %>%
   filter(Date == min) %>%
   mutate(month = month.name[month(min)]) %>%
-  select(month, start_medlemmer = TotalMembers)
+  select(month, start_medlemmer = SamletBestand)
 
-# NB. samme som bestand_måned
+# NB. samme som bestand_md
 slut_medlemmer <- fc_samlet %>%
   group_by(month(Date)) %>%
   mutate(max = max(Date)) %>%
   ungroup() %>%
   filter(Date == max) %>%
   mutate(month = month.name[month(max)]) %>%
-  select(month, slut_medlemmer = TotalMembers)
+  select(month, slut_medlemmer = SamletBestand)
 
 nye_medlemmer <- slut_medlemmer %>%
   mutate(nye_medlemmer = slut_medlemmer - start_medlemmer$start_medlemmer) %>%
@@ -391,9 +394,9 @@ churn <- afgang %>%
   inner_join(start_medlemmer) %>%
   inner_join(slut_medlemmer) %>%
   inner_join(nye_medlemmer) %>%
-  mutate(churn_rate1 = afgang / (start_medlemmer + nye_medlemmer) * 100,
-         churn_rate2 = afgang / (start_medlemmer/2 + slut_medlemmer/2) * 100) %>%
-  select(month,churn_rate1, churn_rate2)
+  mutate(ChurnRate1 = afgang / (start_medlemmer + nye_medlemmer) * 100,
+         ChurnRate2 = afgang / (start_medlemmer/2 + slut_medlemmer/2) * 100) %>%
+  select(month,ChurnRate1, ChurnRate2)
 
 # -----------------------------------------------------------------------------------------
 # Forecast af andel der streamer
@@ -423,33 +426,36 @@ fc_streamers <- fit_streamers %>%
 fc_streamers <- fc_streamers %>%
   as_tibble() %>%
   mutate(month = month.name[month(as.Date(month))]) %>%
-  rename(nStreamers = n)
+  rename(AntalStreamere = n)
 
 # -----------------------------------------------------------------------------------------
 # output
 # -----------------------------------------------------------------------------------------
 
 daily_fc <- fc_betalende %>%
-  mutate(Free = gratis) %>%
+  mutate(Gratis = Gratis) %>%
   inner_join(fc_samlet) %>%
   inner_join(fc_start) %>%
   inner_join(fc_end) %>%
   mutate_if(is.numeric, round)
+ 
 
 monthly <- revenue %>%
   inner_join(streaming_cost) %>%
   inner_join(churn) %>%
-  inner_join(betalende_måned) %>%
-  inner_join(bestand_måned) %>%
-  inner_join(tilgang_måned) %>%
-  inner_join(afgang_måned) %>%
+  inner_join(betalende_md) %>%
+  inner_join(bestand_md) %>%
+  inner_join(tilgang_md) %>%
+  inner_join(afgang_md) %>%
   inner_join(fc_streamers) %>%
-  mutate(AndelStreamers = nStreamers / Bestand * 100) %>%
-  mutate_at(vars(revenue, cost,Betalende,Bestand,tilgang,afgang,nStreamers), round)
+  mutate(AndelStreamere = AntalStreamere / SamletBestand * 100) %>%
+  mutate_at(vars(Revenue, Cost,Betalende,SamletBestand,tilgang,afgang,AntalStreamere), round) %>%
+  mutate_at(vars(ChurnRate1, ChurnRate2,AndelStreamere), round,1)
+
   
 data <- list(Daily = daily_fc, Monthly = monthly)
 
-filename <- glue::glue("forecasts/forecasts_{today()}.xlsx")
+filename <- glue::glue("C:/Users/pech/Desktop/RProjects/ForecastAbo/forecasts/forecasts_{today()}.xlsx")
 
 write.xlsx(data, file = filename)
 
@@ -457,20 +463,30 @@ write.xlsx(data, file = filename)
 # send email
 # -----------------------------------------------------------------------------------------
 
+# insert quote
+quote_df <- read_csv("C:/Users/pech/Desktop/RProjects/ForecastAbo/forecasts/famous_quotes.csv") %>%
+  sample_n(1)
+
+quote <- quote_df$quotes
+author <- quote_df$authors %>% str_remove(",")
+
+quote_string <- paste0(quote," - ",author)
+
 email <- envelope() %>%
   from("pech@saxo.com") %>%
   to("pech@saxo.com") %>% 
   cc("pech@saxo.com") %>%
   subject(glue::glue("Forecasts {today()}")) %>%
-  text("Hej Peder\n\nHer kommer forecasts på Premiumbestanden\nsamt diverse beregninger t.o.m. slutningen af 2020.") %>% 
+  text(glue::glue("Hej Peder\n\nHer kommer nye forecasts for Premiumbestanden samt diverse beregninger t.o.m. slutningen af 2020.\n\n\n{quote_string}")) %>% 
   attachment(filename)
 
 
-creds <- read_rds("email_credentials.rds")
+creds <- read_rds("C:/Users/pech/Desktop/RProjects/ForecastAbo/email_credentials.rds")
 
 smtp <- server(host     = creds[[1]],
                port     = creds[[2]],
                username = creds[[3]],
                password = creds[[4]],)
 
-smtp(email, verbose = TRUE)
+smtp(email, verbose = F)
+
